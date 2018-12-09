37c41ab2a6fcc27d22352c23dac98c84365cd67d
sfattal <fattal.sherif@gmail.com>
2018-12-08 19:53:15 -0500

    switch statement

--- liri.js
+++ liri.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
-var keys = require("keys.js");
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
-// var dotenv = require("dotenv");
 
+// LOGIC
+var command = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+function search(command, input) {
+    switch(command) {
+        case "concert-this":
+        findConcert();
+        break;
+
+        case "spotify-this-song":
+        findSong();
+        break;
+
+        case "movie-this":
+        findMovie();
+        break;
+    }
+}
